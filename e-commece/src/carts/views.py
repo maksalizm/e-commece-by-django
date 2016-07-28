1dad64aaf9b3da676765bce47de74cac6ab051b1
maksalizm <maksalizm@email.com>
2016-07-28 10:08:05 +0900

    0 작업완료
    40 작업완료

--- e-commece/src/carts/views.py
+++ e-commece/src/carts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic.base import View
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+from products.models import Variation
+from carts.models import Cart, CartItem
+
+
+class CartView(View):
+    def get(self, request, *args, **kwargs):
+        request.session.set_expiry(0)  # 300 = 5 minutes
+        cart_id = request.session.get("cart_id")
+        if cart_id == None:
+            cart = Cart()
+            cart.save()
+            cart_id = cart.id
+            request.session["cart_id"] = cart_id
+        cart = Cart.objects.get(id=cart_id)
+        if request.user.is_authenticated():
+            cart.user = request.user
+            cart.save()
+        item_id = request.GET.get("item")
+        delete_item = request.GET.get("delete")
+        if item_id:
+            item_instance = get_object_or_404(Variation, id=item_id)
+            qty = request.GET.get("qty")
+            cart_item = CartItem.objects.get_or_create(cart=cart, item=item_instance)[0]
+            if delete_item:
+                cart_item.delete()
+            else:
+                cart_item.quantity = qty
+                cart_item.save()
+        return HttpResponseRedirect("/")
--- e-commece/src/e-commece/urls.py
+++ e-commece/src/e-commece/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from carts.views import CartView
 
 urlpatterns = [
     # Examples:
@@ -15,9 +16,9 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^products/', include('products.urls')),
     url(r'^categories/', include('products.urls_categories')),
-
+    url(r'^cart/$', CartView.as_view(), name='cart')
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
