0b3def5bf18d06fa64c567d5348f6474c2f8d6b7
maksalizm <maksalizm@email.com>
2016-08-08 10:06:12 +0900

    60작업완료
    60작업완료

--- e-commece/src/e-commece/settings/local.py
+++ e-commece/src/e-commece/settings/local.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = (
     #my apps
     'carts',
     'products',
+    'orders',
     'newsletter',
 )
 
---
+++ e-commece/src/orders/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+class UserCheckout(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, blank=True)  # required
+    email = models.EmailField(unique=True)
+
+    def __unicode__(self):  # when python 3, __str__(self):
+        return self.email
+
+
+ADDRESS_TYPE = (
+    ('billing', 'Billing'),  # firstin database second code
+    ('shipping', 'Shipping'),
+)
+
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(UserCheckout)
+    type = models.CharField(max_length=120, choices=ADDRESS_TYPE)
+    street = models.CharField(max_length=120)
+    city = models.CharField(max_length=120)
+    state = models.CharField(max_length=120)
+    zipcode = models.CharField(max_length=120)
+
+    def __unicode__(self):
+        return self.street
+
+
+
+# class Order(models.Model):
+# cart
+# usercheckout --> required
+# shipping address
+# billing address
+# shipping total price
+# order total (cart total + shipping)
+# order_id --> custom id
